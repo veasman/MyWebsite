d7ed00197c9f9cfdf6ca5b58387acd3195690035
veasman <charlton.moren@gmail.com>
2022-04-11 23:35:23 -0500

    "veasman.github.io -> moren.tech"

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Veasman.Github.Io
+# moren.tech
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.2.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/veasman.github.io'),
+      dir: require('path').join(__dirname, './coverage/moren.tech'),
       subdir: '.',
       reporters: [
         { type: 'html' },
